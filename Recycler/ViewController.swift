98f63c84a752dc658237cd1a3c3d5de0d3e62d82
Przemyslaw Malinowski <przemyslaw.malinowski@linuxpolska.pl>
2019-09-12 12:02:39 +0200

    Initial Commit

---
+++ Recycler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recycler
+//
+//  Created by Przemyslaw Malinowski on 12/09/2019.
+//  Copyright © 2019 Linuxpolska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
